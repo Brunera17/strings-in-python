a662eb54640fbee946ad467fa78f657ff01c7e60
Bruno David Martins <dbrunobruno369@gmail.com>
2025-12-18 11:12:50 -0300

    Substituindo palavras específicas Vol.2

--- substituindo-palavra.py
+++ substituindo-palavra.py
@@ -1,9 +1,16 @@
 frase = input("Digite o texto a ser revisado: ")
 substituir = input("Qual palavra deseja substituir? ")
-novaPalavra = input("Qual a nova palavra? ")
+nova_palavra = input("Qual a nova palavra? ")
 
 if substituir in frase:
-    frase = frase.replace(substituir, novaPalavra)
+    frase = frase.replace(substituir, nova_palavra)
     print(frase)
 else:
     print("Algo deu errado.")
+
+# Versão com Regex
+
+import re
+
+nova_frase = re.sub(rf'\b{substituir}\b', nova_palavra, frase)
+print(nova_frase)
